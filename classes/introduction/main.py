2daaf3fcf3195fafdc6946ff4f15c859b5999fa9
Emerson <emersonfaria019@gmail.com>
2025-12-23 11:44:27 -0300

    feat: sintax

--- classes/introduction/main.py
+++ classes/introduction/main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+# This is my first comment
+"""
+This is my first comment
+This is the second line of comment
+"""
+
+nome = "Emerson" + " Rocha"
+
+idade = 12
+
+if idade < 18:
+    print("Menor de idade")
+else:
+    print("Maior de idade")
