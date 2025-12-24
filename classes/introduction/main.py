5f9229a5f0007a0f6ee75ecdd22cd9fa986ac8b6
Emerson <emersonfaria019@gmail.com>
2025-12-24 12:08:58 -0300

    feat: variables introduction

--- classes/introduction/main.py
+++ classes/introduction/main.py
@@ -1,14 +1,4 @@
-# This is my first comment
-"""
-This is my first comment
-This is the second line of comment
-"""
+nome_completo = "Emerson Rocha"
+idade = 20
 
-nome = "Emerson" + " Rocha"
-
-idade = 12
-
-if idade < 18:
-    print("Menor de idade")
-else:
-    print("Maior de idade")
+nomeCompleto = "Emerson Rocha"
