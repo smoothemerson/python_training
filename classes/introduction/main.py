e69f8e61f33e22ba91e191cc3b08cfcb3261b7b2
emerson <emersonfaria019@gmail.com>
2025-12-25 12:43:01 -0300

    feat: text part 1

--- classes/introduction/main.py
+++ classes/introduction/main.py
@@ -1,23 +1,10 @@
-soma = 1 + 1
-print("1 + 1 = ", soma)
+nome_completo = "Emerson Rocha"
 
-subtracao = 1 - 1
-print("1 - 1 = ", subtracao)
+nome_completo_aspas = """Emerson
+Rocha"""
 
-multiplicacao = 2 * 2
-print("2 x 2 = ", multiplicacao)
+numero_completo_quebra = "Emerson \
+Rocha"
 
-divisao = 5 / 2
-print("5 / 2 = ", divisao)
-print("Tipo da variável do resultado da divisão = ", type(divisao))
-
-print("Valor em inteiro = ", int(divisao))
-
-print("Valor em float = ", float(divisao))
-
-modulo = 5 % 2
-print("Modulo = ", modulo)
-
-divisao = 5 // 2
-print("5 // 2 = ", divisao)
-print("Tipo da variável do resultado da divisão = ", type(divisao))
+nome = "Emerson"
+sobrenome = "Rocha"
