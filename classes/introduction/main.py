ca41c2671979021f6e1ed8f5c5a89742a8570c23
Emerson <emersonfaria019@gmail.com>
2025-12-29 12:20:28 -0300

    feat: booleans

--- classes/introduction/main.py
+++ classes/introduction/main.py
@@ -1,18 +1,20 @@
-nome = "Emerson"
-nome_com_x = "xEmersonx"
-sobrenome = "Rocha"
-nome_completo = "Emerson Rocha"
+if True:
+    print("Bloco IF vai ser executado")
 
-print(f"Contagem do caractere e: {nome_completo.count('e')}")
-print(f"Posição do caractere R: {nome_completo.find('R')}")
-print(f"Encodificação: {nome_completo.encode()}")
-print(f"Decodificação: {nome_completo.encode().decode()}")
-print(f"Substituição do caractere o por 0: {nome_completo.replace('o', '0')}")
-print(f"Adicionar separador entre nome: {'-'.join(nome)}")
-print(f"Dividir nome completo pelo espaço: {nome_completo.split(' ')}")
-print(f"Remover caracteres x do nome alvo: {nome_com_x.strip('x')}")
-print(f"Remover caracteres x da direita do nome alvo: {nome_com_x.rstrip('x')}")
-print(f"Remover caracteres x da esquerda do nome alvo: {nome_com_x.lstrip('x')}")
-print(f"Verifica se o nome completo começa com Eme: {nome_completo.startswith('Eme')}")
-print(f"Verifica se o nome completo contém cha: {'cha' in nome_completo}")
-print(f"Verifica se o nome completo não contém abc: {'abc' not in nome_completo}")
+if True and True:
+    print("Bloco será executado")
+
+if True and False:
+    print("Bloco não será executado")
+
+if False and False:
+    print("Bloco não será executado")
+
+if True or False:
+    print("Bloco OR não vai ser executado")
+
+if False or False:
+    print("Bloco OR não não vai ser executado")
+
+if True or True:
+    print("Bloco OR vai ser executado")
