e88d1425ee1029ad771daf00db324e8b0e20e29b
Emerson <emersonfaria019@gmail.com>
2025-12-29 12:14:43 -0300

    feat: main text-based methods - part 2

--- classes/introduction/main.py
+++ classes/introduction/main.py
@@ -1,21 +1,18 @@
+nome = "Emerson"
+nome_com_x = "xEmersonx"
+sobrenome = "Rocha"
 nome_completo = "Emerson Rocha"
 
-nome_completo_aspas = """Emerson
-Rocha"""
-
-nome_completo_quebra = "Emerson \
-Rocha"
-
-nome = "Emerson"
-sobrenome = "Rocha"
-
-print("Nome completo (1a forma):", nome_completo)
-print("Nome completo (2a forma): " + nome_completo)
-print("Nome completo (3a forma): " + "Emerson " + "Rocha")
-print("Nome completo (4a forma): " + "Emerson", "Rocha")
-print("Nome completo (5a forma): " + nome_completo_aspas)
-print("Nome completo (6a forma): " + nome_completo_quebra)
-print("Nome completo (7a forma): %s" % nome_completo)
-print("Nome completo (8a forma): %s %s" % (nome, sobrenome))
-print(f"Nome completo (9a forma): {nome} {sobrenome}")
-print("Nome completo (9a forma): {} {}".format(nome, sobrenome))
+print(f"Contagem do caractere e: {nome_completo.count('e')}")
+print(f"Posição do caractere R: {nome_completo.find('R')}")
+print(f"Encodificação: {nome_completo.encode()}")
+print(f"Decodificação: {nome_completo.encode().decode()}")
+print(f"Substituição do caractere o por 0: {nome_completo.replace('o', '0')}")
+print(f"Adicionar separador entre nome: {'-'.join(nome)}")
+print(f"Dividir nome completo pelo espaço: {nome_completo.split(' ')}")
+print(f"Remover caracteres x do nome alvo: {nome_com_x.strip('x')}")
+print(f"Remover caracteres x da direita do nome alvo: {nome_com_x.rstrip('x')}")
+print(f"Remover caracteres x da esquerda do nome alvo: {nome_com_x.lstrip('x')}")
+print(f"Verifica se o nome completo começa com Eme: {nome_completo.startswith('Eme')}")
+print(f"Verifica se o nome completo contém cha: {'cha' in nome_completo}")
+print(f"Verifica se o nome completo não contém abc: {'abc' not in nome_completo}")
