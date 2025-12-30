0d7682d3148d3c55e684ec31e2a48c5e532f0b39
Emerson <emersonfaria019@gmail.com>
2025-12-30 11:06:31 -0300

    feat: main methods from the list

--- classes/introduction/main.py
+++ classes/introduction/main.py
@@ -1,8 +1,24 @@
-minha_lista = [1, 2, 3, 4, 5, "rocketseat", True, False]
+minha_lista = [1, 2, 3, 4, 5]
+print("Minhas lista de exemplo:", minha_lista)
 
-print("Minha lista de exemplo", minha_lista)
-print("minha_lista[0]:", minha_lista[0])
-print("minha_lista[5]:", minha_lista[5])
-print("minha_lista[1:7]:", minha_lista[1:7])
-print("minha_lista[:6]:", minha_lista[:6])
-print("minha_lista[2:]:", minha_lista[2:])
+minha_lista[0] = 1
+print("Minhas lista de exemplo:", minha_lista)
+
+minha_lista.append(6)
+print("Após append(6):", minha_lista)
+
+indice = minha_lista.index(6)
+print("Índice do elemento 6:", indice)
+
+minha_lista.insert(2, 10)
+print("Após insert(2, 10):", minha_lista)
+
+elemento_removido = minha_lista.pop(3)
+print("Elemento removido:", elemento_removido)
+print("Após pop(3):", minha_lista)
+
+minha_lista.remove(True)
+print("Após remove(True):", minha_lista)
+
+minha_lista.sort()
+print("Após sort():", minha_lista)
