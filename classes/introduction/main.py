6fb034b41a8b62c99cd0ee81425dbfedea853481
Emerson <emersonfaria019@gmail.com>
2026-01-02 15:53:22 -0300

    feat: main dictionary methods

--- classes/introduction/main.py
+++ classes/introduction/main.py
@@ -7,3 +7,18 @@ print("Cidade:", pessoa["cidade"])
 
 pessoa["sobrenome"] = "Silva"
 print("Sobrenome:", pessoa["sobrenome"])
+
+pessoa["idade"] = 31
+print("Idade atualizada:", pessoa["idade"])
+
+del pessoa["cidade"]
+print("Dicionário após remover a cidade:", pessoa)
+
+chaves = list(pessoa.keys())
+print("Chaves do dicionário:", chaves)
+
+valores = list(pessoa.values())
+print("Valores do dicionário:", valores)
+
+itens = list(pessoa.items())
+print("Pares chave-valor do dicionário:", itens)
