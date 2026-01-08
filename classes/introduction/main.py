ac783acb41fee76c28dbd0cd5dbe2f68ff0dfcbb
Emerson <emersonfaria019@gmail.com>
2026-01-08 14:19:43 -0300

    feat: project functions - viewing tasks

--- classes/introduction/main.py
+++ classes/introduction/main.py
@@ -1,8 +1,16 @@
 def adicionar_tarefa(tarefas, nome_tarefa):
-    tarefa = {"nome": nome_tarefa, "completada": False}
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
     tarefas.append(tarefa)
     print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
     return
+
+
+def ver_tarefas(tarefas):
+    print("\nLista de tarefas:")
+    for indice, tarefa in enumerate(tarefas, start=1):
+        status = "✅" if tarefa["completada"] else " "
+        nome_tarefa = tarefa["tarefa"]
+        print(f"{indice}. [{status}] {nome_tarefa}")
 
 
 tarefas = []
@@ -21,6 +29,8 @@ while True:
     if escolha == "1":
         nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
         adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "2":
+        ver_tarefas(tarefas)
     elif escolha == "6":
         break
 
