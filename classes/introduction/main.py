ecd1fbca87640b36a1b4bae70e7437c81a3cd1a5
Emerson <emersonfaria019@gmail.com>
2026-01-08 14:12:13 -0300

    feat: roles in the project - adding tasks

--- classes/introduction/main.py
+++ classes/introduction/main.py
@@ -1,3 +1,12 @@
+def adicionar_tarefa(tarefas, nome_tarefa):
+    tarefa = {"nome": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+    return
+
+
+tarefas = []
+
 while True:
     print("\nMenu do Gerenciador de Lista de tarefas:")
     print("1. Adicionar tarefa")
@@ -9,7 +18,10 @@ while True:
 
     escolha = input("Digite sua escolha: ")
 
-    if escolha == "6":
+    if escolha == "1":
+        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+        adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "6":
         break
 
 print("Programa finalizado")
