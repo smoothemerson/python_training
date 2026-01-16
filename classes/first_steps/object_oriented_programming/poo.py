a8d77a19c1c94fcfa8126650a948cceb07fc4b3f
Emerson <emersonfaria019@gmail.com>
2026-01-16 11:38:44 -0300

    feat: What is OOP - Part 2

--- classes/first_steps/object_oriented_programming/poo.py
+++ classes/first_steps/object_oriented_programming/poo.py
@@ -2,3 +2,15 @@ class Pessoa:
     def __init__(self, nome, idade) -> None:
         self.nome = nome
         self.idade = idade
+
+    def saudacao(self):
+        return f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos."
+
+
+pessoa1 = Pessoa("Alice", 30)
+mensage1 = pessoa1.saudacao()
+print(mensage1)
+
+pessoa2 = Pessoa("Rodrigo", 32)
+mensage2 = pessoa2.saudacao()
+print(mensage2)
