bffebe683b5b159f1c38b9223dea6422b7127d6b
Emerson <emersonfaria019@gmail.com>
2026-01-22 12:26:52 -0300

    feat: Creating the project classes

---
+++ classes/first_steps/object_oriented_programming/projeto/jogo.py
@@ -0,0 +1,48 @@
+class Personagem:
+    def __init__(self, nome, vida, nivel) -> None:
+        self.__nome = nome
+        self.__vida = vida
+        self.__nivel = nivel
+
+    def get_nome(self):
+        return self.__nome
+
+    def get_vida(self):
+        return self.__vida
+
+    def get_nivel(self):
+        return self.__nivel
+
+    def exibir_detalhes(self):
+        return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
+
+
+class Heroi(Personagem):
+    def __init__(self, nome, vida, nivel, habilidade) -> None:
+        super().__init__(nome, vida, nivel)
+        self.__habilidade = habilidade
+
+    def get_habilidade(self):
+        return self.__habilidade
+
+    def exibir_detalhes(self):
+        return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
+
+
+class Inimigo(Personagem):
+    def __init__(self, nome, vida, nivel, tipo) -> None:
+        super().__init__(nome, vida, nivel)
+        self.__tipo = tipo
+
+    def get_tipo(self):
+        return self.__tipo
+
+    def exibir_detalhes(self):
+        return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}\n"
+
+
+heroi = Heroi("Herói", 100, 5, "Super Força")
+print(heroi.exibir_detalhes())
+
+inimigo = Inimigo("Morcego", 50, 3, "Voador")
+print(inimigo.exibir_detalhes())
