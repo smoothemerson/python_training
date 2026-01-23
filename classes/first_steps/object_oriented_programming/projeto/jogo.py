179d4e9942f372b378dc167ac7b80ede7bc02e9e
Emerson <emersonfaria019@gmail.com>
2026-01-23 12:22:16 -0300

    feat: Creating the combat mechanics

--- classes/first_steps/object_oriented_programming/projeto/jogo.py
+++ classes/first_steps/object_oriented_programming/projeto/jogo.py
@@ -16,6 +16,16 @@ class Personagem:
     def exibir_detalhes(self):
         return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
 
+    def atacar(self, alvo):
+        dano = self.__nivel * 2
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
+
+    def receber_ataque(self, dano):
+        self.__vida -= dano
+        if self.__vida < 0:
+            self.__vida = 0
+
 
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade) -> None:
@@ -58,6 +68,16 @@ class Jogo:
 
             input("Pressione Enter para atacar...")
             escolha = input("Escolha (1 - Ataque Normal, 2 - Ataque Especial): ")
+
+            if escolha == "1":
+                self.heroi.atacar(self.inimigo)
+            else:
+                print("Escolha inválida. Escolha novamente.")
+
+        if self.heroi.get_vida() > 0:
+            print("\nParabéns, você venceu a batalha!")
+        else:
+            print("\nVocê foi derrotado!")
 
 
 jogo = Jogo()
