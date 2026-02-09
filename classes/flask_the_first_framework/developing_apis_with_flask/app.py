f5fd88922f9b4bffb1527520e67ba91c1f5cb743
Emerson <emersonfaria019@gmail.com>
2026-02-09 13:15:53 -0300

    feat: CRUD Development with Flask - Update

--- classes/flask_the_first_framework/developing_apis_with_flask/app.py
+++ classes/flask_the_first_framework/developing_apis_with_flask/app.py
@@ -41,5 +41,23 @@ def get_task(id):
     return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
 
+@app.route("/tasks/<int:id>", methods=["PUT"])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if not task:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+    data = request.get_json()
+    task.title = data["title"]
+    task.description = data["description"]
+    task.completed = data["completed"]
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
